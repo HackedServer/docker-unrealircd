ba51b10499e87497ebb912312aeff460a9d6214e
HackedServer <hs@hackedserver.net>
2026-03-16 20:55:19 -0700

    Emit logs to stdout

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 if [ "$1" = "start" ]; then
   ./unrealircd start
+  tail -F logs/ircd.log &
+  TAIL_PID=$!
   while kill -0 $(cat data/unrealircd.pid) 2>/dev/null; do
     sleep 5
   done
+  kill $TAIL_PID
   exit 1
 else
   exec ./unrealircd "$@"
